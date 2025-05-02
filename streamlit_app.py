59d395ab61038b5477854e4bba381f985acfb798
dalya2004 <dalyashakra0@gmail.com>
2025-05-02 14:21:28 +0000

    defined my travel columns and added a sidebar for my planned pages

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,17 @@
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+df = pd.read_csv("inddata.csv")
+#defining the travel columns used in the data 
+travel_columns = [
+    'Bus (type not known)', 'Car Share', 'Car/Van', 'Cycle',
+    'Dedicated School Bus', 'Public Bus Service', 'Taxi', 'Train', 'Walk'
+]
+
+# Sidebar Navigation
+page = st.sidebar.radio(
+    "Navigation",
+    ["Overview", "Sustainability Insights", "Low Sustainability Schools", "Average Transport Mode"]
 )
+
