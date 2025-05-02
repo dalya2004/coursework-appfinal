a518a4082629d5454dbfa91dcc1580907919c219
dalya2004 <dalyashakra0@gmail.com>
2025-05-02 14:40:58 +0000

    added some key metrics for the school chosen by the user to show the top trasnport mode

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,3 +28,19 @@ if page == "Overview":
     #displays the data
     st.subheader(f"Travel Data for {selected_school}")
     st.write(filtered_df)
+
+    #Checking if the data is available for the selected school
+if not filtered_df.empty:
+    #Get the travel data for that school 
+    travel_counts = filtered_df[travel_columns].iloc[0].dropna()
+    total_students = int(travel_counts.sum())
+
+    #Finds the top transport mode
+    top_mode = travel_counts.idxmax()
+    top_value = int(travel_counts.max())
+
+    #shows key stats using metrics
+    st.subheader("Key Stats")
+    col1, col2 = st.columns(2)
+    col1.metric("Total Students", f"{total_students}")
+    col2.metric("Top Transport Mode", f"{top_mode}", f"{top_value} students")
